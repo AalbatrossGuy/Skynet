f63fdd1a78f5bf9bf0358b89c8449b22508e1e1a
AalbatrossGuy <thexcelsiorisback@gmail.com>
2025-10-16 18:09:42 +0530

    Added type hints

--- client/data.py
+++ client/data.py
@@ -1,15 +1,21 @@
 import numpy
 
-def generate_dataset_local(n, data, seed, prevalence: float = 0.12):
+
+def generate_dataset_local(
+        n: int,
+        data: int,
+        seed: int | None,
+        prevalence: float = 0.12
+):
     generate_random_seed = numpy.random.RandomState(seed)
-    X_matrix = generate_random_seed.normal(0, 1, size = (n, data))
-    weight = generate_random_seed.normal(0, 0.7, size = (data, ))
+    X_matrix = generate_random_seed.normal(0, 1, size=(n, data))
+    weight = generate_random_seed.normal(0, 0.7, size=(data, ))
     log_value = X_matrix @ weight
     y: int = (generate_random_seed.rand(n) < prevalence * (1 / 1 + numpy.exp(-log_value))).astype(int)
-    
+
     for _ in range(int(n * 0.02)):
         index_1, index_2 = generate_random_seed.randint(0, n), generate_random_seed.randint(0, data)
         X_matrix[index_1, index_2] += generate_random_seed.uniform(3, 6)
         y[index_1] = 1
-        
+
     return X_matrix, y
